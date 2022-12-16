2ca925d53aebd5fab418c5b947d16bd3ad614c6e
Swathi P <bhatswathip@gmail.com>
2022-12-16 21:49:34 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css'
 
 export default function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div>
                 <Switch>
                     <Route exact path="/" component={ LandingPage } />
