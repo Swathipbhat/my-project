7304ff6b1bea0e9f7dc5306bbf5ee6fb35b3bec3
Swathi P <bhatswathip@gmail.com>
2022-12-16 21:30:46 +0530

    Updates

---
+++ precache-manifest.04caf65976100880a60394a5d3cf0854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec0285c81775550d47e69d3d2c5c821",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "03327fa3d9002f4481d3",
+    "url": "/my-project/static/css/main.4112a3ac.chunk.css"
+  },
+  {
+    "revision": "dacb60858011e5e74690",
+    "url": "/my-project/static/js/2.49d27a67.chunk.js"
+  },
+  {
+    "revision": "03327fa3d9002f4481d3",
+    "url": "/my-project/static/js/main.9133236f.chunk.js"
+  },
+  {
+    "revision": "02869910549d422a2b1d",
+    "url": "/my-project/static/js/runtime~main.8ce5ffd9.js"
+  },
+  {
+    "revision": "c00ac42cf96f4fe10204a03d5220675c",
+    "url": "/my-project/static/media/bg.c00ac42c.png"
+  }
+]);
