c029203a7012181531c43f6d6fd277eef89bbe99
Swathi P <bhatswathip@gmail.com>
2022-12-16 22:40:55 +0530

    Create a change

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -7,8 +7,8 @@ export default function SignInPage() {
     return (
         <div className="text-center m-5-auto">
             <div className="overlay">
-           
-            <form action="/home">
+         
+            <form action="https://Swathipbhat.github.io/my-project/home">
             <div className="con">
     
             <header className="head-form">
