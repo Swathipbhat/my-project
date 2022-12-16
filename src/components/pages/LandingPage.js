080f119e9e63cd9ebf16367c581673a836010c64
Swathi P <bhatswathip@gmail.com>
2022-12-16 22:30:01 +0530

    Create a change

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -5,24 +5,21 @@ import '../../App.css'
 import BackgroundImage from '../../assets/images/bg.png'
 
 export default function LandingPage() {
-    const history = createHistory({
-        basename: process.env.PUBLIC_URL,
-      });
-      const store = configureStore({ history });
+  
     return (
        
         <header style={ HeaderStyle }>
             <h1 className="main-title text-center">login / register page</h1>
             <p className="main-para text-center">Check out Contacts List Available</p>
             <div className="buttons text-center">
-            <ConnectedRouter history={history}>
+         
                 <Link to="/login">
                     <button className="primary-button">log in</button>
                 </Link>
                 <Link to="/register">
                     <button className="primary-button" id="reg_btn"><span>register </span></button>
                 </Link>
-                </ConnectedRouter>
+           
             </div>
         </header>
     )
